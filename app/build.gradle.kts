5455b2765c61648f42d60775f3f9165cc2133b5d
shivkadari200626 <shivkadari@gmail.com>
2026-07-07 00:24:36 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,12 +26,9 @@ android {
         }
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    compilerOptions {
-    jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    
+    kotlinOptions {
+    jvmTarget = "17"
 }
     buildFeatures {
         viewBinding = true
