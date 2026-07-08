cca9ad7c57a50d94a6a73d43c9ba59287a903c3d
shivkadari200626 <shivkadari@gmail.com>
2026-07-08 07:32:33 +0530

    Create RetrofitClient.kt

---
+++ app/src/main/kotlin/com/virtualoutfittryon/RetrofitClient.kt
@@ -0,0 +1,6 @@
+package com.virtualoutfittryon
+
+// You can delete this if you're not using it, or keep for future
+object RetrofitClient {
+    // Empty for now, we use GeminiApi object directly
+}
