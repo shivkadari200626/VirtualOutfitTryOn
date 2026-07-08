63e649b4f338305435442229bf5c6c43c418827f
shivkadari200626 <shivkadari@gmail.com>
2026-07-08 07:03:18 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.virtualoutfittryon"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.virtualoutfittryon"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
         ndk { abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a")) }
