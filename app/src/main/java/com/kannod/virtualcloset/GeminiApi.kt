39b8c0cd9df3063563a2719a5c3699d2a49529a0
shivkadari200626 <shivkadari@gmail.com>
2026-07-08 06:34:06 +0530

    Update GeminiApi.kt

--- app/src/main/java/com/kannod/virtualcloset/GeminiApi.kt
+++ app/src/main/java/com/kannod/virtualcloset/GeminiApi.kt
@@ -1,8 +1,8 @@
-package com.virtualoutfittryon  // <-- CHANGE THIS
+package com.virtualoutfittryon  // NEW
 
 import com.virtualoutfittryon.databinding.ActivityMainBinding
 import com.virtualoutfittryon.databinding.ActivityResultBinding
-
+import com.virtualoutfittryon.BuildConfig
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
