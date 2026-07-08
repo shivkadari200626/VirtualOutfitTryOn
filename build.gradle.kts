f1ef471ac4f22d9b0064cf4291f6f20f10d78dd3
shivkadari200626 <shivkadari@gmail.com>
2026-07-08 06:43:41 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,8 @@
+// Top-level build file
 plugins {
     alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
+// DO NOT add repositories here. 
+// They are already in settings.gradle.kts
